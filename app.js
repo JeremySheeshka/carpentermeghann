cd0a1fc1aa18bdfcd8056b57e042f4d9620f79ba
Jeremy Sheeshka <Jsheeshka@gmail.com>
2025-01-11 13:28:29 -0800

    testing

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
    const contactForm = document.querySelector('.contact-form');
+ 
+   console.log(document.querySelector('.contact-form'));
 
    // Input elements of form
    let userName = document.getElementById('name');
    let email = document.getElementById('email');
    let subject = document.getElementById('subject');
    let message = document.getElementById('message');
-
-   contactForm.addEventListener('submit', (e) => {
+ 
+   if (contactForm && userName && email && subject && message) {
+     contactForm.addEventListener('submit', (e) => {
        e.preventDefault();
-
+ 
        let formData = {
-           name: userName.value,
-           email: email.value,
-           subject: subject.value,
-           message: message.value,
+         name: userName.value,
+         email: email.value,
+         subject: subject.value,
+         message: message.value,
        };
-
+ 
        let xhr = new XMLHttpRequest();
        xhr.open('POST', '/');
        xhr.setRequestHeader('content-type', 'application/json');
        xhr.onload = function() {
-           console.log(xhr.responseText);
-           if (xhr.responseText == 'success') {
-               alert('Email sent');
-               userName.value = '';
-               email.value = '';
-               subject.value = '';
-               message.value = '';
-           } else {
-               alert('Something went wrong!');
-           }
+         console.log(xhr.responseText);
+         if (xhr.responseText === 'success') {
+           alert('Email sent');
+           userName.value = '';
+           email.value = '';
+           subject.value = '';
+           message.value = '';
+         } else {
+           alert('Something went wrong!');
+         }
        };
-
+ 
        xhr.send(JSON.stringify(formData));
-   });
-});
+     });
+   } else {
+     console.error('One or more form elements are null.');
+   }
+ });
+ 
