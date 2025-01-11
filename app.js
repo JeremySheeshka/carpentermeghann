33652ac8990df6723cd01d571f5e37f9020b78c0
Jeremy Sheeshka <Jsheeshka@gmail.com>
2025-01-11 12:56:31 -0800

    test

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
+
 // document.addEventListener('DOMContentLoaded', () => {
-   const contactForm = document.querySelector('.contact-form');
+     const contactForm = document.querySelector('.contact');
 
-   // Input elements of form
-   let userName = document.getElementById('name'); // Renamed variable to avoid conflict
-   let email = document.getElementById('email');
-   let subject = document.getElementById('subject');
-   let message = document.getElementById('message');
+        // Input elements of form
+        let userName = document.getElementById('name'); // Renamed variable to avoid conflict
+        let email = document.getElementById('email');
+        let subject = document.getElementById('subject');
+        let message = document.getElementById('message');
 
-//  if (contactForm && userName && email && subject && message) {
-     contactForm.addEventListener('submit', (e)=>{
-           e.preventDefault();
+      //  if (contactForm && userName && email && subject && message) {
+          contactForm.addEventListener('submit', (e)=>{
+                e.preventDefault();
+                
+          
+             let formData = {
+                  name: name.value,
+                  email: email.value,
+                  subject: subject.value,
+                  message: message.value,
+                }            
 
-        let formData = {
-             name: userName.value,
-             email: email.value,
-             subject: subject.value,
-             message: message.value,
-           }            
-
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', '/');
-        xhr.setRequestHeader('content-type', 'application/json');
-        xhr.onload = function() {
-             console.log(xhr.responseText);
-             if(xhr.responseText === 'success'){
-             alert('Email sent');
-             userName.value = '';
-             email.value = '';
-             subject.value = '';
-             message.value = '';
-
-            } else {
-                alert('Something went wrong!');
+             let xhr = new XMLHttpRequest();
+             xhr.open('POST', '/');
+             xhr.setRequestHeader('content-type', 'application/json');
+             xhr.onload = function() {
+                  console.log(xhr.responseText);
+                  if(xhr.responseText == 'success'){
+                  alert('Email sent');
+                  userName.value = '';
+                  email.value = '';
+                  subject.value = '';
+                  message.value = '';
+            
+                 } else {
+                     alert('Something went wrong!')
+                 }
             }
-       }
 
-       xhr.send(JSON.stringify(formData));
-
-       
-  //     });
-//    } else {
-//       console.error('One or more form elements are null.');
-//     }
- })
+            xhr.send(JSON.stringify(formData));
+       //     });
+    //    } else {
+     //       console.error('One or more form elements are null.');
+   //     }
+      })
